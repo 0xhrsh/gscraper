c87fa930ace106974362529fd994f3ade67f3152
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-05-09 01:31:19 +0530

    fixed channels getting blocked

--- gsc.go
+++ gsc.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -25,16 +24,39 @@ type App struct {
 
 var rApps int
 var wApps int
+var naApps int
+
+func writeToCSV(AppsInfo chan App, w *csv.Writer) {
+	for app := range AppsInfo {
+		w.Write([]string{fmt.Sprint(wApps + 1), app.name, app.publisher, app.installs, app.adds, app.genre, app.ratings, app.url})
+		w.Flush()
+		wApps++
+		println(rApps, wApps, naApps)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Panicln(err)
+	}
+}
 
 func main() {
 	rApps = 0
 	wApps = 0
+	naApps = 0
 
-	AppsInfo := make(chan App, 500)
-	Urls := make(chan string, 5000)
-	TUrls := make(chan string, 5000)
+	AppsInfo := make(chan App, 1000)
+	Urls := make(chan string, 1000)
+	NextUrls := make(chan string, 3000)
+
 	urlStore := make(map[string]bool)
 	mapMutex := sync.RWMutex{}
+
+	file, _ := os.OpenFile("Data.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
 	var seed [37]string
 	seed[0] = "https://play.google.com/store/apps/top"
@@ -75,144 +97,103 @@ func main() {
 	seed[35] = "https://play.google.com/store/apps/category/GAME_RACING"
 	seed[36] = "https://play.google.com/store/apps/category/BEAUTY"
 
-	// mapMutex.Lock()
-	// urlStore[seed1] = true
-	// urlStore[seed2] = true
-	// urlStore[seed3] = true
-	// urlStore[seed4] = true
-	// urlStore[seed5] = true
-	// urlStore[seed6] = true
-	// mapMutex.Unlock()
-	for i := 0; i < 37; i++ {
-		Urls <- seed[i]
+	go func() {
+		for i := 0; i < 37; i++ {
+			Urls <- seed[i]
+
+		}
+	}()
+
+	for i := 0; i < 5000; i++ {
+		go func() {
+			for url := range Urls {
+				getNextUrls(url, NextUrls, urlStore, &mapMutex) // go to each url to get NextUrls
+			}
+		}()
 	}
 
-	// Urls <- "https://play.google.com" + seed2
-	// Urls <- "https://play.google.com" + seed3
-	// Urls <- "https://play.google.com" + seed4
-	// Urls <- "https://play.google.com" + seed5
-	// Urls <- "https://play.google.com" + seed6
-	// Urls <- "https://play.google.com" + seed7
-	// Urls <- "https://play.google.com" + seed8
+	for i := 0; i < 5000; i++ {
+		go func() {
+			for url := range NextUrls {
+				getAppInfo(url, AppsInfo, Urls) // go to each url to get info and find more urls
+			}
+		}()
 
-	file, _ := os.OpenFile("Data.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-
-	defer file.Close()
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	for i := 0; i < 200; i++ {
-		go getAppInfo(Urls, TUrls, AppsInfo, urlStore, &mapMutex)
 	}
 
-	for i := 0; i < 250; i++ {
-		go getUrls(Urls, TUrls, urlStore, &mapMutex)
-	}
+	go writeToCSV(AppsInfo, writer) // write the Apps in AppsInfo to a csv file
 
-	for i := 0; i < 1; i++ {
-		go writeToCSV(AppsInfo, writer)
-	}
-
-	var first string
-	fmt.Scanln(&first)
-
-	time.Sleep(2 * time.Second)
-	fmt.Println("here==>",<-TUrls)
-	fmt.Println("here==>",<-Urls)
-	fmt.Println("here==>",<-AppsInfo)
-	// close(Urls)
-	// close(TUrls)
-	// close(AppsInfo)
-	// time.Sleep(2 * time.Second)
+	var inp string
+	fmt.Scanln(&inp)
 
 }
 
-func writeToCSV(AppsInfo chan App, w *csv.Writer) {
-	for app := range AppsInfo {
-		w.Write([]string{fmt.Sprint(wApps + 1), app.name, app.publisher, app.installs, app.adds, app.genre, app.ratings, app.url})
-		w.Flush()
-		println(rApps, wApps)
-		wApps++
+func getNextUrls(url string, NextUrls chan string, urlStore map[string]bool, mapMutex *sync.RWMutex) {
 
-	}
-}
+	resp, err := http.Get(url)
+	checkError(err)
 
-func getUrls(Urls chan string, TUrls chan string, urlStore map[string]bool, mapMutex *sync.RWMutex) {
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	checkError(err)
 
-	for url := range Urls {
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Panicln(err)
-		}
-		time.Sleep(600 * time.Microsecond)
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
-		if err != nil {
-			log.Panicln(err)
-		}
-
-		doc.Find("a.poRVub").Each(
-			func(i int, s *goquery.Selection) {
-				next, ok := s.Attr("href")
+	doc.Find("a.poRVub").Each(
+		func(i int, s *goquery.Selection) {
+			next, ok := s.Attr("href")
+			mapMutex.Lock()
+			_, prs := urlStore[next]
+			mapMutex.Unlock()
+			if ok && !prs {
 				mapMutex.Lock()
-				_, prs := urlStore[next]
+				urlStore[next] = true
 				mapMutex.Unlock()
-				if ok && !prs {
-					mapMutex.Lock()
-					urlStore[next] = true
-					mapMutex.Unlock()
 
-					TUrls <- "https://play.google.com" + next
+				select {
+				case NextUrls <- "https://play.google.com" + next:
+					//
+				default:
+					//
 				}
-			})
-		time.Sleep(10 * time.Millisecond)
-	}
-	// return
-}
 
-func getAppInfo(Urls chan string, TUrls chan string, AppsInfo chan App, urlStore map[string]bool, mapMutex *sync.RWMutex) {
-
-	for url := range TUrls {
-
-		Urls <- url
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Panicln(err)
-			// return
-		}
-		time.Sleep(800 * time.Microsecond)
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
-		if err != nil {
-			log.Panicln(err)
-			// return
-		}
-
-		app := App{}
-		var info [2]string
-
-		app.name = doc.Find("h1.AHFaub").Text()
-		doc.Find("span.T32cc.UAO9ie").Each(func(i int, s *goquery.Selection) {
-			if i < 2 {
-				info[i] = s.Text()
 			}
 		})
-		app.publisher = info[0]
-		app.genre = info[1]
-		app.ratings = doc.Find("div.BHMmbe").Text()
-		app.adds = doc.Find("div.bSIuKf").Text()
-		app.installs = doc.Find("span.EymY4b").Text()
-		app.url = url
 
-		if app.name == "" {
-			mapMutex.Lock()
-			delete(urlStore, url)
-			mapMutex.Unlock()
-			TUrls <- url
+}
+
+func getAppInfo(url string, AppsInfo chan App, Urls chan string) {
+
+	resp, err := http.Get(url)
+	checkError(err)
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	checkError(err)
+
+	app := App{}
+	var info [2]string
+	app.name = doc.Find("h1.AHFaub").Text()
+	doc.Find("span.T32cc.UAO9ie").Each(func(i int, s *goquery.Selection) {
+		if i < 2 {
+			info[i] = s.Text()
 		}
+	})
+	app.publisher = info[0]
+	app.genre = info[1]
+	app.ratings = doc.Find("div.BHMmbe").Text()
+	app.adds = doc.Find("div.bSIuKf").Text()
+	app.installs = doc.Find("span.EymY4b").Text()
+	app.url = url
 
-		AppsInfo <- app
+	if app.name == "" {
+		naApps++
+	} else {
 		rApps++
-		
-		time.Sleep(400 * time.Millisecond)
-
 	}
+	AppsInfo <- app
+
+	select {
+	case Urls <- url:
+		//
+	default:
+		//
+	}
+
 }
