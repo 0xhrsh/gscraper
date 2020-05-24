15695311de18c16890af171526261701429822ab
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-05-24 10:24:54 +0530

    added additional fields

--- scrapApp.go
+++ scrapApp.go
@@ -12,13 +12,18 @@ import (
 
 // App contains app information
 type App struct {
-	Name      string `json:"Name"`
-	Ratings   string `json:"Ratings"`
-	Adds      string `json:"Adds"`
-	Publisher string `json:"Publisher"`
-	Installs  string `json:"Installs"`
-	Genre     string `json:"Genre"`
-	URL       string `json:"Url"`
+	Name        string `json:"Name"`
+	Ratings     string `json:"Ratings"`
+	Adds        string `json:"Adds"`
+	Publisher   string `json:"Publisher"`
+	PublisherID string `json:"PublisherID"`
+	Installs    string `json:"Installs"`
+	Genre       string `json:"Genre"`
+	URL         string `json:"Url"`
+	Email       string `json:"Email"`
+	Updated     string `json:"Updated"`
+	Size        string `json:"Size"`
+	Logo        string `json:"Logo"`
 }
 
 var rApps int
@@ -55,16 +60,34 @@ func getAppInfo(url string, AppsInfo chan App, Urls chan string, NextUrls chan s
 	var info [2]string
 	app.Name = doc.Find("h1.AHFaub").Text()
 	doc.Find("span.T32cc.UAO9ie").Each(func(i int, s *goquery.Selection) {
+		if i == 0 {
+			app.PublisherID = s.AttrOr("href", "404")
+		}
 		if i < 2 {
 			info[i] = s.Text()
 		}
 	})
+	doc.Find("div.hAyfc").Each(func(i int, s *goquery.Selection) {
+
+		if s.Find("div.BgcNfc").Text() == "Updated" {
+			s.Find("span.htlgb").Each(func(x int, in *goquery.Selection) {
+				app.Updated = in.Text()
+			})
+		} else if s.Find("div.BgcNfc").Text() == "Size" {
+			s.Find("span.htlgb").Each(func(x int, in *goquery.Selection) {
+				app.Size = in.Text()
+			})
+		}
+
+	})
 	app.Publisher = info[0]
 	app.Genre = info[1]
+	app.Logo = doc.Find("img.T75of.sHb2Xb").AttrOr("src", "404")
 	app.Ratings = doc.Find("div.BHMmbe").Text()
 	app.Adds = doc.Find("div.bSIuKf").Text()
 	app.Installs = doc.Find("span.EymY4b").Text()
 	app.URL = url
+	app.Email = doc.Find("a.hrTbp.euBY6b").Text()
 	rApps++
 	if app.Name == "" && app.Publisher == "" {
 		naApps++
