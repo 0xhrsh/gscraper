c153f7d0d9ac16583524f49741c8e5ceb8b53f17
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-05-24 11:06:02 +0530

    fixed publisher url

--- scrapApp.go
+++ scrapApp.go
@@ -61,7 +61,7 @@ func getAppInfo(url string, AppsInfo chan App, Urls chan string, NextUrls chan s
 	app.Name = doc.Find("h1.AHFaub").Text()
 	doc.Find("span.T32cc.UAO9ie").Each(func(i int, s *goquery.Selection) {
 		if i == 0 {
-			app.PublisherID = s.AttrOr("href", "404")
+			app.PublisherID = s.Find("a.hrTbp.R8zArc").AttrOr("href", "404")
 		}
 		if i < 2 {
 			info[i] = s.Text()
